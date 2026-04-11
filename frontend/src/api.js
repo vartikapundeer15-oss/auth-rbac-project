f3eede086ef18b246f855dea43c71044705614fa
Vartika Pundeer <vartikapundeer15@gmail.com>
2026-04-12 00:53:36 +0530

    Fix API for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://your-backend.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {

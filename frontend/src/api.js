b92af21ab35ac181fcf427a95d28e3b378969ef5
Vartika Pundeer <vartikapundeer15@gmail.com>
2026-04-12 17:21:36 +0530

    Fixed API URL properly

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://auth-rbac-project.onrender.com
+  baseURL: "https://auth-rbac-project.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
